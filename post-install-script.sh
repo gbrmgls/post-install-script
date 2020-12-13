b1ec5c6eb5df662310bca643ad9f1b38a62773b1
Gabriel Magalhães <31325728+gbrmgls@users.noreply.github.com>
2020-12-13 19:43:16 -0300

    Update post-install-script.sh

--- post-install-script.sh
+++ post-install-script.sh
@@ -60,7 +60,7 @@ qbittorrent \
 # -----------------------------------------
 # FLATPAKS
 
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo -y
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install flathub -y
 sudo flatpak install discord runelite drawing yaru-dark qbittorrent -y
 
