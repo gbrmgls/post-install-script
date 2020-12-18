f94027b4edd177f23bfe67aa32da4964c74c3acc
Gabriel Magalhães <31325728+gbrmgls@users.noreply.github.com>
2020-12-18 20:40:31 -0300

    Update post-install-script.sh

--- post-install-script.sh
+++ post-install-script.sh
@@ -17,9 +17,6 @@ wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > pa
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
-## joplin
-wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash
-
 ## zerotier-one
 curl -s https://install.zerotier.com | sudo bash
 
@@ -65,7 +62,7 @@ qbittorrent \
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install flathub -y
-sudo flatpak install discord runelite drawing yaru-dark -y
+sudo flatpak install discord runelite drawing yaru-dark obsidian -y
 
 #------------------------------------------
 # SNAPS
