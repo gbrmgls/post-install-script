845757c5452169a35f4055e2a07b0189d4b77467
Gabriel Magalhães <31325728+gbrmgls@users.noreply.github.com>
2020-12-24 00:04:18 -0300

    Update post-install-script.sh

--- post-install-script.sh
+++ post-install-script.sh
@@ -18,6 +18,10 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
+   
+## docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 ## vscode
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
