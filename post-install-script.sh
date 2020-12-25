b80e2bd6a88c199ee2a0685425e735e819ccaecd
Gabriel Magalhães <31325728+gbrmgls@users.noreply.github.com>
2020-12-25 17:02:04 -0300

    Update post-install-script.sh

--- post-install-script.sh
+++ post-install-script.sh
@@ -17,7 +17,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
-   stable"
+   stable" -y
    
 ## docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
